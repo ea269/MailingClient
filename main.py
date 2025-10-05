e16b3f88f6e09529ed801a80cfe0a8d2ddb7081e
Emmanuel Andrade <emmanuelandrade189@gmail.com>
2025-10-05 00:03:37 -0700

    Renamed variables for easier reading, and fixed inccorect usage

--- main.py
+++ main.py
@@ -19,17 +19,17 @@ with open('credentials.txt', 'r') as f:
 server.login(email, password)
 
 # Message headers
-message = multipart.MIMEMultipart()
-message['From'] = 'dad'
-message['To'] = to
-message['Subject'] = 'Testing 1 2 3.'
+mail = multipart.MIMEMultipart()
+mail['From'] = 'dad'
+mail['To'] = to
+mail['Subject'] = 'Testing 1 2 3.'
 
 # Message body
 with open('mail.txt', 'r') as f:
-    mail = f.read()
+    body = f.read()
 
 # Combine them
-message.attach(text.MIMEText(message, 'plain'))
+mail.attach(text.MIMEText(body, 'plain'))
 
 # Image
 filename = 'stern_dad.jpg'
@@ -42,8 +42,8 @@ p.set_payload(attachment.read())
 # Encode 
 encoders.encode_base64(p)
 p.add_headers('Content-Disposition', f'attachment; filename={filename}')
-message.attach(p)
+mail.attach(p)
 
 # Convert to string
-text = msg.as_string()
+text = mail.as_string()
 server.sendmail(email, to, text)
